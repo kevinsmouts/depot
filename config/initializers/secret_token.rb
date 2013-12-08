f5ddbf7eeca68d93c0c169a70df51f0084329579
Kevin Smouts <kevin.smouts@gmail.com>
2013-12-08 15:31:58 -0800

    add and setup figaro

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Depot::Application.config.secret_key_base = 'a638596f7b04e827f8e588d93a8d976b277b6ae4256a950125aa3f24ea000f949a1d0cc677a5ef7da74a0fe2c173eaf3995fa49df565a8f9d80a027e04ec83e9'
+Depot::Application.config.secret_key_base = ENV["SECRET_TOKEN"]
